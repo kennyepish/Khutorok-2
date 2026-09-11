6b3a2dad7541f682d45fdd670170ee6ac7f72d3d
kennyepish <kenny.epish@gmail.com>
2026-09-11 17:15:08 +0300

    Add files via upload

---
+++ Tileset/Autotile_Grass_and_Dirt_Path_Tileset.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Autotile_Grass_and_Dirt_Path_Tileset" tilewidth="16" tileheight="16" tilecount="231" columns="21">
+ <image source="../Tileset/Autotile_Grass_and_Dirt_Path_Tileset.png" width="336" height="176"/>
+ <wangsets>
+  <wangset name="Луг, поле, вода" type="mixed" tile="-1">
+   <wangcolor name="Луг" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Поле" color="#aa5500" tile="-1" probability="1"/>
+   <wangcolor name="Вода" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="34" wangid="3,3,3,3,3,3,3,3"/>
+   <wangtile tileid="36" wangid="1,1,2,2,2,1,1,1"/>
+   <wangtile tileid="37" wangid="1,1,2,2,2,2,2,1"/>
+   <wangtile tileid="38" wangid="1,1,1,1,2,2,2,1"/>
+   <wangtile tileid="39" wangid="2,2,2,1,2,2,2,2"/>
+   <wangtile tileid="40" wangid="2,2,2,2,2,1,2,2"/>
+   <wangtile tileid="57" wangid="2,2,2,2,2,1,1,1"/>
+   <wangtile tileid="58" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="59" wangid="2,1,1,1,2,2,2,2"/>
+   <wangtile tileid="60" wangid="2,1,2,2,2,2,2,2"/>
+   <wangtile tileid="61" wangid="2,2,2,2,2,2,2,1"/>
+   <wangtile tileid="78" wangid="2,2,2,1,1,1,1,1"/>
+   <wangtile tileid="79" wangid="2,2,2,1,1,1,2,2"/>
+   <wangtile tileid="80" wangid="2,1,1,1,1,1,2,2"/>
+   <wangtile tileid="81" wangid="2,2,2,1,2,2,2,1"/>
+   <wangtile tileid="82" wangid="2,1,2,2,2,1,2,2"/>
+   <wangtile tileid="127" wangid="3,3,3,3,1,3,3,3"/>
+   <wangtile tileid="128" wangid="3,3,1,3,1,3,3,3"/>
+   <wangtile tileid="129" wangid="3,3,1,3,1,3,1,3"/>
+   <wangtile tileid="130" wangid="3,3,3,3,1,3,1,3"/>
+   <wangtile tileid="131" wangid="1,3,1,3,1,3,1,1"/>
+   <wangtile tileid="132" wangid="3,3,1,1,1,3,1,3"/>
+   <wangtile tileid="133" wangid="3,3,1,3,1,1,1,3"/>
+   <wangtile tileid="134" wangid="1,1,1,3,1,3,1,3"/>
+   <wangtile tileid="135" wangid="3,3,1,1,1,3,3,3"/>
+   <wangtile tileid="136" wangid="1,3,1,1,1,1,1,3"/>
+   <wangtile tileid="137" wangid="3,3,1,1,1,1,1,3"/>
+   <wangtile tileid="138" wangid="3,3,3,3,1,1,1,3"/>
+   <wangtile tileid="148" wangid="1,3,3,3,1,3,3,3"/>
+   <wangtile tileid="149" wangid="1,3,1,3,1,3,3,3"/>
+   <wangtile tileid="150" wangid="1,3,1,3,1,3,1,3"/>
+   <wangtile tileid="151" wangid="1,3,3,3,1,3,1,3"/>
+   <wangtile tileid="152" wangid="1,3,1,1,1,3,3,3"/>
+   <wangtile tileid="153" wangid="1,1,1,1,1,1,1,3"/>
+   <wangtile tileid="154" wangid="1,3,1,1,1,1,1,1"/>
+   <wangtile tileid="155" wangid="1,3,3,3,1,1,1,3"/>
+   <wangtile tileid="156" wangid="1,1,1,1,1,3,3,3"/>
+   <wangtile tileid="157" wangid="1,1,1,3,1,1,1,3"/>
+   <wangtile tileid="159" wangid="1,3,1,3,1,1,1,1"/>
+   <wangtile tileid="169" wangid="1,3,3,3,3,3,3,3"/>
+   <wangtile tileid="170" wangid="1,3,1,3,3,3,3,3"/>
+   <wangtile tileid="171" wangid="1,3,1,3,3,3,1,3"/>
+   <wangtile tileid="172" wangid="1,3,3,3,3,3,1,3"/>
+   <wangtile tileid="173" wangid="1,1,1,3,1,3,3,3"/>
+   <wangtile tileid="174" wangid="1,1,1,1,1,3,1,1"/>
+   <wangtile tileid="175" wangid="1,1,1,3,1,1,1,1"/>
+   <wangtile tileid="176" wangid="1,3,3,3,1,3,1,1"/>
+   <wangtile tileid="177" wangid="1,1,1,1,1,3,1,3"/>
+   <wangtile tileid="178" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="179" wangid="1,3,1,1,1,3,1,1"/>
+   <wangtile tileid="180" wangid="1,3,3,3,1,1,1,1"/>
+   <wangtile tileid="191" wangid="3,3,1,3,3,3,3,3"/>
+   <wangtile tileid="192" wangid="3,3,1,3,3,3,1,3"/>
+   <wangtile tileid="193" wangid="3,3,3,3,3,3,1,3"/>
+   <wangtile tileid="194" wangid="1,3,1,3,1,1,1,3"/>
+   <wangtile tileid="195" wangid="1,1,1,3,3,3,1,3"/>
+   <wangtile tileid="196" wangid="1,3,1,3,3,3,1,1"/>
+   <wangtile tileid="197" wangid="1,3,1,1,1,3,1,3"/>
+   <wangtile tileid="198" wangid="1,1,1,3,3,3,3,3"/>
+   <wangtile tileid="199" wangid="1,1,1,3,3,3,1,1"/>
+   <wangtile tileid="200" wangid="1,1,1,3,1,3,1,1"/>
+   <wangtile tileid="201" wangid="1,3,3,3,3,3,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
