18e0d2ea155f5bf63ae638a302f3c7941d81bddc
kennyepish <kenny.epish@gmail.com>
2026-09-17 19:27:13 +0300

    Add files via upload

---
+++ Tileset/Buildings.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Buildings" tilewidth="256" tileheight="256" tilecount="5" columns="0">
+ <tile id="0">
+  <image source="addon_store.png" width="256" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="farmers_store.png" width="144" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="garage.png" width="144" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="barn.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="tractor_store.png" width="256" height="256"/>
+ </tile>
+</tileset>
